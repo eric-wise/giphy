da200e3a7368e710c7d55443f7e537727a4c5e54
Eric Wise <ericwise@gmail.com>
2021-06-01 13:59:07 -0500

    env

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Giphy = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+        params: {
+          api_key: process.env.REACT_APP_API_KEY,
+        },
+      });
+      console.log(results.data);
+    };
+    fetchData();
+  });
+
   return <div className="">Gliphy</div>;
 };
 
