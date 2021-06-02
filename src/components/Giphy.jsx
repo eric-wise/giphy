0e174058486d51710e0c81702e21b820d6693008
Eric Wise <ericwise@gmail.com>
2021-06-01 21:20:10 -0500

    SearchBox

--- src/components/Giphy.jsx
+++ src/components/Giphy.jsx
@@ -1,20 +1,117 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 const Giphy = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
-      const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+          params: {
+            api_key: process.env.REACT_APP_API_KEY,
+          },
+        });
+
+        console.log(results);
+        setData(results.data.data);
+      } catch (err) {
+        setIsError(true);
+        setTimeout(() => setIsError(false), 4000);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const renderGifs = () => {
+    if (isLoading) {
+      return <Loading />;
+    }
+    return data.map((el) => {
+      return (
+        <div key={el.id} className="gif">
+          <img src={el.images.fixed_height.url} />
+        </div>
+      );
+    });
+  };
+
+  const renderError = () => {
+    if (isError) {
+      return (
+        <div
+          className="alert alert-danger alert-dismissible fade show"
+          role="alert"
+        >
+          Unable to get Gifs, please try again in a few minutes
+        </div>
+      );
+    }
+  };
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsError(false);
+    setIsLoading(true);
+
+    try {
+      const results = await axios("https://api.giphy.com/v1/gifs/search", {
         params: {
-          api_key: process.env.REACT_APP_API_KEY,
+          api_key: "tAEFUgagRjRNkU24orQdFB8EHMcNTUSe",
+          q: search,
+          limit: 100,
         },
       });
-      console.log(results.data);
-    };
-    fetchData();
-  });
+      setData(results.data.data);
+    } catch (err) {
+      setIsError(true);
+      setTimeout(() => setIsError(false), 4000);
+    }
 
-  return <div className="">Gliphy</div>;
+    setIsLoading(false);
+  };
+
+  //   const pageSelected = (pageNumber) => {
+  //     setCurrentPage(pageNumber);
+  //   };
+
+  return (
+    <div className="m-2">
+      {renderError()}
+      <form className="form-inline justify-content-center m-2">
+        <input
+          value={search}
+          onChange={handleSearchChange}
+          type="text"
+          placeholder="search"
+          className="form-control"
+        />
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="btn btn-primary mx-2"
+        >
+          Go
+        </button>
+      </form>
+
+      <div className="container gifs">{renderGifs()}</div>
+    </div>
+  );
 };
 
 export default Giphy;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loader">
+      <i className="fas fa-spinner fa-4x fa-spin" />
+    </div>
+  );
+};
+export default Loading;
